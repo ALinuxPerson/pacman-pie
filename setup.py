f0fc5004a8db3956a9c3a7d15309e4b922e82a6b
ALinuxPerson <micheal02052007@gmail.com>
2020-06-29 20:53:41 +0800

    Add temporary setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from pacmanpie.__version__ import __version__ as version
+import setuptools
+
+setuptools.setup(
+    name="pacman-pie",
+    author="ALinuxPerson",
+    description="A pythonic implementation of Arch Linux's pacman using pyalpm.",
+    version=version
+)
