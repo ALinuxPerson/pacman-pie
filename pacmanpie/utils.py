39e3245de026b800605b94e27c234cf041a86f56
ALinuxPerson <micheal02052007@gmail.com>
2020-07-01 16:45:40 +0800

    Remove generate_repr

--- pacmanpie/utils.py
+++ pacmanpie/utils.py
@@ -20,10 +20,3 @@ class RichArgumentParser(argparse.ArgumentParser):
 def _generate_variable_expression(name: str, value: Any) -> str:
     value = f"'{value}'" if isinstance(str, value) else value
     return f"{name}={value}"
-
-
-def generate_repr(self) -> str:
-    class_name: str = self.__class__.__name__
-    class_items: ItemsView[str, Any] = self.__dict__.items()
-    return \
-        f"<{class_name} object: {' '.join(_generate_variable_expression(name, value) for name, value in class_items if not name.startswith('_'))}>"
